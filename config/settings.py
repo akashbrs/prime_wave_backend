ac7b15e6ef0b4c445586c3d9ebddf0b5893d977a
akashbrs <methukuakash092@gmail.com>
2026-04-03 22:52:13 +0530

    fix cors middleware

--- config/settings.py
+++ config/settings.py
@@ -47,9 +47,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.security.SecurityMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  # ✅ FIXED
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,13 +130,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "https://primewave-organisation.vercel.app"
-]
-# ✅ CORS SETTINGS (FINAL FIX)
-
 CORS_ALLOW_ALL_ORIGINS = True
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://primewave-organisation.vercel.app",
+]
 
 CORS_ALLOW_HEADERS = [
     "*"
